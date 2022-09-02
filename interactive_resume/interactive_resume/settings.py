1dd3691e94740ae2842ea783b20af7f8226c8298
Kaleb-Abiy <kalebabiy2012@gmail.com>
2022-09-02 22:42:17 +0300

    secret key hidden

--- interactive_resume/interactive_resume/settings.py
+++ interactive_resume/interactive_resume/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import environ
 from pathlib import Path
+
+env =  environ.Env()
+
+# reading .env file
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&=ld&9xo-)n6py^=d)ipn03mb9edpfj&d%i1g_r-gv%%64&=8u'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
